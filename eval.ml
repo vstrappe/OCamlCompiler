866e1e9e8992796b359f752b199cdefc584ddb31
vstrappe <123578964+vstrappe@users.noreply.github.com>
2023-08-11 15:06:28 -0400

    Update eval.ml

--- eval.ml
+++ eval.ml
@@ -5,21 +5,10 @@ exception TypeError of string
 exception DeclareError of string
 exception DivByZeroError 
 
-(* Provided functions
-  Helpful for creating an environment. You can do this with references 
-  (not taught) or without. 
-  You do not have to use these and you can modify them if you want. 
-  If you do not use references, you will need the following data type:
-*)
-(*type values = Int of int|Bool of bool|String of string*)
-
-(* Adds mapping [x:v] to environment [env] *)
 let ref_extend env x v = (x, ref v)::env
 
 let extend env x v = (x,v)::env
 
-(* Returns [v] if [x:v] is a mapping in [env]; uses the
-   most recent if multiple mappings for [x] are present *)
 let rec ref_lookup env x =
   match env with
   | [] -> raise (DeclareError ("Unbound variable " ^ x))
@@ -30,25 +19,17 @@ let rec lookup env x =
   [] -> raise (DeclareError ("Unbound variable " ^ x))
   | (var, value)::t -> if x = var then value else lookup t x
 
-(* Creates a placeholder mapping for [x] in [env]; needed
-   for handling recursive definitions *)
 let ref_extend_tmp env x = (x, ref (Int 0))::env
 
-(* Updates the (most recent) mapping in [env] for [x] to [v] *)
 let rec ref_update env x v =
   match env with
   | [] -> raise (DeclareError ("Unbound variable " ^ x))
   | (var, value)::t -> if x = var then (value := v) else ref_update t x v
         
-(* Removes the most recent variable,value binding from the environment *)
 let rec remove env x = match env with
   [] -> []
   | (var,value)::t -> if x = var then t else (var,value)::(remove t x)
 
-(* Part 1: Evaluating expressions *)
-
-(* Evaluates MicroCaml expression [e] in environment [env],
-   returning a value, or throwing an exception on error *)
 let rec eval_expr env e = match e with
         | Value v -> v
         | ID i -> ref_lookup env i
@@ -112,12 +93,6 @@ let rec eval_expr env e = match e with
                 | Closure (a, x, e) -> let b = eval_expr env v in eval_expr (ref_extend a x b) e
                 | _ -> raise (TypeError "Not a function"))
 
-
-(* Part 2: Evaluating mutop directive *)
-
-(* Evaluates MicroCaml mutop directive [m] in environment [env],
-   returning a possibly updated environment paired with
-   a value option; throws an exception on error *)
 let eval_mutop env m = match m with
         | Def (id, e) -> let n = (ref_extend_tmp env id) in let v = eval_expr n e in let () = ref_update n id v in (n, Some(v))
         | Expr e -> let exp = eval_expr env e in (env, Some(exp))
